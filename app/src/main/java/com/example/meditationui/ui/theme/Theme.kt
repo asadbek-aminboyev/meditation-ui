f50a9c54fd50e3b2d7e4c5eda4e546396a59af80
Asadbek <aaminboyev2@gmail.com>
2023-02-06 15:06:46 +0300

    init

---
+++ app/src/main/java/com/example/meditationui/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.example.meditationui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = DeepBlue,
+    primaryVariant = ButtonBlue,
+    secondary = LightGreen1
+)
+
+private val LightColorPalette = lightColors(
+    primary = DeepBlue,
+    primaryVariant = ButtonBlue,
+    secondary = LightGreen1
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun MeditationUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
